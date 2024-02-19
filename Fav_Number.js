5bdeca4b5470372d6d40370d8693408e25df2070
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 12:38:05 -0800

    Add files via upload

---
+++ Fav_Number.js
@@ -0,0 +1,4 @@
+"use strict";
+const fav_Number = 28;
+const message = 'Here is my fav number ' + fav_Number;
+console.log(message);
---
+++ Fav_Number.ts
@@ -0,0 +1,5 @@
+const fav_Number :number = 28 ;
+
+const message :string ='Here is my fav number ' + fav_Number;
+
+console.log(message) ;
